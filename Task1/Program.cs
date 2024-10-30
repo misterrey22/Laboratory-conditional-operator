62ade28e09fcd0d1b1a564ee4013c077887fcf3c
misterrey <maksropcan@gmail.com>
2024-10-30 19:09:02 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("Введіть ціле число: ");
+        int num = int.Parse(Console.ReadLine());
+        if (num == 0)
+        {
+            Console.WriteLine("Дорівнює нулю");
+        }
+        else if (num > 0)
+        {
+            Console.WriteLine("Число додатнє");
+        }
+        else
+        {
+            Console.WriteLine("Число від'ємне");
+        }
---
+++ Task8/Program.cs
@@ -0,0 +1,27 @@
+﻿Напишіть програму, яка визначає, яка з введених користувачем сторін трикутника (a, b, c) є найдовшою.
+        Console.Write("a = ");
+int number1_ = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+int number2_ = int.Parse(Console.ReadLine());
+Console.Write("c = ");
+int number3_ = int.Parse(Console.ReadLine());
+
+int maxSide = Max(number1_, number2_, number3_);
+Console.WriteLine($"Найбільша сторона: {maxSide}");
+
+static int Max(int a, int b, int c)
+{
+    int max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    return max;
+}
+
+    }
+}
